761118933636bbb40fd87c448c971ae379ea5b63
Epat <kamikadze2185@gmail.com>
2018-04-07 00:38:27 +0200

    Table parser is done
    aby sparsować plan lekcji wywołaj
    'zaladujPlan(URL)'
    URL, to lokalna ścieżka do pliku HTML,
    NIE MOŻNA wpływać na zawartość ramki, jeśli plik html nie jest przechowywany lokalnie.
    Po zakończeniu parsowania ramka jest usuwana, a plan jest dostępny w zmiennej "plan"

--- temp/parseTable/parse.js
+++ temp/parseTable/parse.js
@@ -1,10 +1,60 @@
-const iframe = document.getElementById('tPlan'); //pobranie ramki
+// aby sparsować plan lekcji wywołaj
+// 'zaladujPlan(URL)'
+// URL, to lokalna ścieżka do pliku HTML,
+// NIE MOŻNA wpływać na zawartość ramki, jeśli plik html nie jest przechowywany lokalnie.
+// Po zakończeniu parsowania ramka jest usuwana, a plan jest dostępny w zmiennej "plan"
 
-function parsujPlan() {
-  let tabela = iframe.contentWindow.document.querySelector('.tabela');
-  console.log(tabela);
-  console.log(tabela.children);
-
+// Tworzy ramkę i ładuje plan do niej
+function zaladujPlan(url) {
+  let iframe = document.createElement('iframe');
+  iframe.src = url;
+  iframe.id = 'tPlan';
+  iframe.style.display = 'none';
+  document.body.appendChild(iframe);
+  iframe.addEventListener('load', parsujPlan);
 }
 
-parsujPlan();
+let plan;
+
+function parsujPlan() {
+  const iframe = document.getElementById('tPlan');
+  let tabela = iframe.contentWindow.document.querySelector('.tabela'); //wczytanie tabelki z ramki
+  let arr = [];
+
+  let rows = tabela.children[0].children; //pobranie wierszy tabelki
+
+  for (i = 0; i < rows.length; i++) { // dodanie pojedyńczych elementów do arr
+    let tempArr = [];
+    for (j = 0; j < rows[i].children.length; j++) {
+
+      if (rows[i].children[j].innerHTML != '&nbsp;') {
+        tempArr.push(rows[i].children[j].innerHTML);
+      } else {
+        tempArr.push('');
+      }
+    }
+
+    arr.push(tempArr);
+  }
+
+  let tempArr = new Array(arr[0].length); //przygotowanie tabeli do przegrupowania elementów
+  for (var i = 0; i < tempArr.length; i++) {
+    tempArr[i] = new Array(arr.length);
+  }
+
+  for (i = 0; i < arr.length; i++) { //grupowanie elementów kolumnami, zamiast wierszami
+    for (j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] != '&nbsp;') {
+        tempArr[j][i] = arr[i][j];
+      } else {
+        tempArr[j][i] = '';
+      }
+
+    }
+  }
+
+  arr = tempArr;
+  plan = arr;
+  iframe.parentNode.removeChild(iframe); //usunięcie ramki
+  return arr;
+}
