1fb64585a9e15eb761706397f82a3cfd111f7e37
Jakub Szymański <jakubmateusz@poczta.onet.pl>
2018-04-30 12:29:59 +0200

    moved download to function

--- Project-N/www/scripts/index.js
+++ Project-N/www/scripts/index.js
@@ -19,10 +19,32 @@ function onDeviceReady() {
     initDzien();
     initP();
 
+    downloadPlans();
     zaladujPlan(cordova.file.dataDirectory + `plan/o6.html`);
     
 
 };
+
+function downloadPlans() {
+    downloader.init({folder: "plan", fileSystem:cordova.file.dataDirectory , delete: false});
+    downloader.getMultipleFiles([{url:"http://www.zse.zary.pl/plan/plany/o1.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o2.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o3.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o4.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o5.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o6.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o7.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o8.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o9.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o10.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o11.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o12.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o13.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o14.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o15.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o16.html"},
+                                {url:"http://www.zse.zary.pl/plan/plany/o17.html"}]);
+}
 
 function onPause() {
     // TODO: Ta aplikacja została zawieszona, Zapisz tutaj stan aplikacji. (np. powrót do ekranu głównego)
