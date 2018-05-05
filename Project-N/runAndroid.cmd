9dc0eaddc2cd7c4063ff616186f48a23fa718273
Epat <kamikadze2185@gmail.com>
2018-05-05 02:43:48 +0200

    compile and launch android apk
    select architecture, compile, install selected apk and launch it

---
+++ Project-N/runAndroid.cmd
@@ -0,0 +1,56 @@
+@echo off
+
+IF "%~1"=="/h" (
+  call :HELP
+)
+
+IF "%~1"=="" (
+  call :HELP
+)
+
+IF "%~1"=="arm64" (
+  SET arch=arm64
+  goto RUN
+)
+
+IF "%~1"=="armeabi" (
+  SET arch=armeabi
+  goto RUN
+)
+
+IF "%~1"=="armv7" (
+  SET arch=armv7
+  goto RUN
+)
+
+IF "%~1"=="x86" (
+  SET arch=x86
+  goto RUN
+)
+
+IF "%~1"=="x86_64" (
+  SET arch=x86_64
+  goto RUN
+)
+
+:HELP
+echo "Usage: %~nx0 <architecture>"
+echo.
+echo "Example: %~nx0 arm64"
+echo.
+echo.
+echo "Available architectures: arm64, armeabi, armv7, x86, x86_64"
+exit /B
+
+
+
+:RUN
+echo.
+echo "==COMPILING APKS=="
+powershell "cordova compile android"
+echo.
+echo "==INSTALLING APK=="
+powershell "adb install .\platforms\android\app\build\outputs\apk\%arch%\debug\app-%arch%-debug.apk"
+echo.
+echo "==LAUNCHING APK=="
+powershell "adb shell am start -n io.project.n/io.project.n.MainActivity"
